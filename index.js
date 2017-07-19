d3379436ffa403976cad26cad5c0e9eed58c515c
Arnel T. Lenteria <arnel.lenteria@gmail.com>
2017-07-19 20:00:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 /* Imports */
 const repl = require("repl")
 const facebook = require("facebook-chat-api")
+const exec = require('child_process').exec;
 
 /* Globals */
 let api = {}
@@ -23,12 +24,6 @@ const commandMap = {
 	"m": commandEnum.MESSAGE
 }
 
-const facebookStickers = { //packid -> sticker id
-	"227877430692340": {
-		"369239263222822": ":thumbsup:"
-	}
-}
-
 /* Initialisation */
 if (process.argv.length < 3) {
 	//	User didn't store credentials in JSON, make them manually enter credentials
@@ -42,7 +37,7 @@ if (process.argv.length < 3) {
 	}, {
 		name: "password",
 		hidden: true,
-		conform: () => true
+		conform() { return true }
 	}], (err, result) => { authenticate(result) })
 
 } else {
@@ -63,7 +58,7 @@ function getUserDetails() {
 		api.getFriendsList((err, data) => {
 			if (err) {
 				console.error(err)
-				reject(err)
+				reject()
 			}
 			user.friendsList = data
 			resolve()
@@ -72,59 +67,42 @@ function getUserDetails() {
 }
 
 /**
- * Returns the user info for a given userID.
- */
-function getUser(userID) {
-	const _user = user.friendsList.find(f => f.userID === userID)
-	if (!_user) {
-		api.getUserInfo(userID, (err, data) => {
-			if (err) return console.error(err)
-
-			data[userID].userID = Object.keys(data)[0]
-			user.friendsList.push(data[userID])
-			getUser(userID)
-		})
-	} else {
-		return _user
-	}
-}
-
-/**
  * Handles incoming messages by logging appropriately.
  */
 function handleMessage(message) {
+	const unrenderableMessage = "https://www.messenger.com/m/"+message.messageID
+
 	// seen message (not sent)
-	if (!message.senderID || message.type !== "message")
+	if (!message.senderID || message.type != "message")
 		return
 
-	let senderObj = getUser(message.senderID)
-	let sender = senderObj.fullName || senderObj.name || "Unknown User"
-	if (!senderObj.isFriend)
-		sender += " [not your friend]"
+	let sender = user.friendsList.find(f => { return f.userID === message.senderID })
+	if (!!!sender){
+		return false;
+	}
+
+	sender = sender.fullName || "Unknown User"
 
 	if (message.participantNames && message.participantNames.length > 1)
-		sender = `'${sender}' (${message.senderName})`
+		sender = "'" + sender + "'" + " (" + message.senderName + ")"
 
-	process.stderr.write("\x07")	// Terminal notification
+	if (lastThread == message.threadID){
+		process.stderr.write("\x07")	// Terminal notification
+	}else{
+		exec("say new message from "+sender);
+	}
 
 	let messageBody = null
 
 	if (message.body !== undefined && message.body != "") {
+		// console.log("New message sender " + sender + " - " + message.body)
 		messageBody = message.body
-	} else {
-		messageBody = "unrenderable in Messer :("
 	}
 
-	if (message.attachments.length === 0) {
-		console.log(`New message from ${sender} - ${messageBody}`)
+	if (message.attachments.length == 0) {
+		console.log("░░░ "+sender + ": "  + (messageBody || unrenderableMessage) + "\n")
 	} else {
-		const attachment = message.attachments[0] // only first attachment
-		const attachmentType = attachment.type.replace(/\_/g, " ")
-
-		if (attachmentType === "sticker")
-			messageBody = facebookStickers[attachment.packID][attachment.stickerID] || messageBody
-
-		console.log(`New ${attachmentType} from ${sender} - ${messageBody}`)
+		console.log("░░░ "+sender + ": \n" + (messageBody || unrenderableMessage)+ "\n")
 	}
 
 	lastThread = message.threadID
@@ -135,19 +113,24 @@ const commands = {
   /**
    * Sends message to given user
    */
-	[commandEnum.MESSAGE](rawCommand) {
+	message(rawCommand) {
 		const quoteReg = /(".*?")(.*)/g
 		// to get length of first arg
 		const args = rawCommand.replace("\n", "").split(" ")
-		const cmd = rawCommand.substring(args[0].length).trim()
+		let cmd = rawCommand.substring(args[0].length).trim()
 
 		if (cmd.match(quoteReg) == null) {
-			console.warn("Invalid message - check your syntax")
-			return processCommand("help")
+			let possibleRecipient = ((cmd || "").split(" ")[0]);
+			if (possibleRecipient.length > 0){
+				cmd = cmd.replace(possibleRecipient, '"'+possibleRecipient+'"')
+			}else{
+				console.warn("Invalid message - check your syntax")
+				return processCommand("help")
+			}
 		}
 
 		const decomposed = quoteReg.exec(cmd)
-		const rawReceiver = decomposed[1].replace(/"/g, "")
+		const rawReceiver = decomposed[1].replace(/"/g, "") || ""
 		const message = decomposed[2].trim()
 
 		if (message.length == 0) {
@@ -157,55 +140,51 @@ const commands = {
 
 		// Find the given reciever in the users friendlist
 		const receiver = user.friendsList.find(f => {
-			return f.fullName.toLowerCase().startsWith(rawReceiver.toLowerCase())
+			return f.fullName.toLowerCase().startsWith(rawReceiver.toLowerCase()) || ((f.vanity || "").toLowerCase() == rawReceiver.toLowerCase())
 		})
 
 		if (!receiver) {
-			console.warn(`User '${rawReceiver}' could not be found in your friends list!`)
+			console.warn("User \"" + rawReceiver + "\"" + " could not be found in your friends list!")
 			return
 		}
 
-		api.sendMessage(message, receiver.userID, err => {
-			if (err) return console.error("ERROR:", err.error)
-
-			console.log(`Sent message to ${receiver.fullName}`)
+		api.sendMessage(message, receiver.userID, (err, res) => {
+			lastThread = res.threadID || res.messageID
+			if (err) console.warn("ERROR!", err)
+			console.log(" ---------- "+receiver.fullName+" ----------")
+			console.log("░░░ You: " + message + "\n>")
 		})
 	},
 
   /**
    * Replies with a given message to the last received thread.
    */
-	[commandEnum.REPLY](rawCommand) {
+	reply(rawCommand) {
 		if (lastThread === null) {
-			return console.warn("Error - can't reply to messages you haven't yet received! You need to receive a message before using `reply`!")
+			console.warn("Error - can't reply to messages you haven't yet received! You need to receive a message before using `reply`!")
 		}
 
 		const args = rawCommand.replace("\n", "").split(" ")
 		const body = rawCommand.substring(args[0].length).trim()
 
 		// var body = rawCommand.substring(commandEnum.REPLY.length).trim()
-
 		api.sendMessage(body, lastThread, err => {
-			if (err) return console.error("ERROR:", err.error)
-
-			console.log("✓")
+			if (err) return console.error(err)
+			console.log("░░░ You: "+body);
 		})
 	},
 
   /**
    * Displays users friend list
    */
-	[commandEnum.CONTACTS]() {
-		if (user.friendsList.length === 0) {
-			console.log("You have no friends :cry:")
-		}
+	contacts() {
 		user.friendsList.forEach(f => { console.log(f.fullName) })
 	},
 
   /**
    * Displays usage instructions
    */
-	[commandEnum.HELP]() {
+	help() {
 		console.log("Commands:\n" +
 			"\tmessage \"[user]\" [message]\n" +
 			"\tcontacts\n"
@@ -217,15 +196,12 @@ const commands = {
  * Execute appropriate action for user input commands
  */
 function processCommand(rawCommand) {
-	// skip if command is only spaces
-	if (rawCommand.trim().length === 0) return
-
 	const args = rawCommand.replace("\n", "").split(" ")
 	const command = commandMap[args[0]] || args[0]
 	const commandHandler = commands[command]
 
 	if (!commandHandler) {
-		console.error("Invalid command - check your syntax")
+		console.error("Invalid command - check your syntax\n>")
 	} else {
 		commandHandler(rawCommand)
 	}
@@ -233,19 +209,19 @@ function processCommand(rawCommand) {
 
 function authenticate(credentials) {
 	facebook(credentials, (err, fbApi) => {
-		if (err) return
+		if (err) return console.error(err)
 
 		api = fbApi // assign to global variable
 		api.setOptions({ logLevel: "silent" })
 
-		console.info(`Logged in as ${credentials.email}`)
+		console.info("Logged in as " + credentials.email)
 
 		getUserDetails(api, user).then(() => {
 			console.info("Listening for incoming messages...")
 
 			// listen for incoming messages
 			api.listen((err, message) => {
-				if (err) return
+				if (err) return console.error(err)
 				handleMessage(message)
 			})
 
